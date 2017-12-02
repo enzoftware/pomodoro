f4f6c791c1d9faca6694c802ae7e5d19c9529628
enzoftware <lizama.enzo@gmail.com>
2017-12-02 02:29:34 -0500

    Final commit :boom:

--- app/src/main/java/com/projects/enzoftware/countdowntimer/MainActivity.kt
+++ app/src/main/java/com/projects/enzoftware/countdowntimer/MainActivity.kt
@@ -2,16 +2,20 @@ package com.projects.enzoftware.countdowntimer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+var isRunning = false
+var timeMil : Long = 0
+var countDownTimer : CountDownTimer ?= null
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var isRunning = false
-        var time = 0
+
+
 
         imageViewSwitch.setOnClickListener {
             if (!isRunning){
@@ -29,17 +33,39 @@ class MainActivity : AppCompatActivity() {
             stopCounting()
             isRunning = false
             imageViewSwitch.setImageResource(R.drawable.play)
-
+            textViewCount!!.text="" + timeMil / 1000
+            progressBar.progress = timeMil.toInt() / 1000
+            progressBar.max = timeMil.toInt() / 1000
         }
 
 
     }
 
     private fun stopCounting() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        imageViewSwitch.setImageResource(R.drawable.play)
+        isRunning = false
+        countDownTimer!!.cancel()
+
     }
 
     private fun startCounting() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val txtInput = editTextCount!!.text.toString()
+        val timeInput = txtInput.toLong() * 1000
+        timeMil = timeInput
+        progressBar.max = timeMil.toInt() / 1000
+        imageViewSwitch.setImageResource(R.drawable.stop)
+        isRunning = true
+        countDownTimer = object  : CountDownTimer(timeMil,1000){
+            override fun onFinish() {
+
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                textViewCount.text = Math.round(millisUntilFinished * 0.001f).toString()
+                progressBar.progress = Math.round(millisUntilFinished * 0.001f)
+            }
+        }.start()
+
+        countDownTimer!!.start()
     }
 }
