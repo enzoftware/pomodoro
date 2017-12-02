b81d2544cfb1a0353cd9d3609aea7cb6215ba031
enzoftware <lizama.enzo@gmail.com>
2017-12-02 01:15:24 -0500

    Resources

--- app/src/main/java/com/projects/enzoftware/countdowntimer/MainActivity.kt
+++ app/src/main/java/com/projects/enzoftware/countdowntimer/MainActivity.kt
@@ -2,11 +2,44 @@ package com.projects.enzoftware.countdowntimer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var isRunning = false
+        var time = 0
+
+        imageViewSwitch.setOnClickListener {
+            if (!isRunning){
+                if (editTextCount!!.text.toString().isEmpty()){
+                    Toast.makeText(this,"Tiempo vacio",Toast.LENGTH_SHORT).show()
+                }else{
+                    startCounting()
+                }
+            }else{
+                stopCounting()
+            }
+        }
+
+        imageViewReset.setOnClickListener {
+            stopCounting()
+            isRunning = false
+            imageViewSwitch.setImageResource(R.drawable.play)
+
+        }
+
+
+    }
+
+    private fun stopCounting() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun startCounting() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
